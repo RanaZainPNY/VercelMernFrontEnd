3e62581e0b4301da9839ae6075d003c1cf67b780
Rana Zain Idrees <rzi.codealigned@gmail.com>
2023-08-22 11:06:45 +0500

    capitalize

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React App is working Fine</h1>
+      <h1>Hello React App is Working Fine</h1>
     </div>
   );
 }
